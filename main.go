d9a2f3a8c6e526fafaf2cdf5a98e93cdfe3c64c7
Andrew Cleland <andrew.cleland3@gmail.com>
2018-09-30 16:44:30 +1000

    Convert tabs to spaces

--- main.go
+++ main.go
@@ -1,106 +1,106 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"time"
+    "fmt"
+    "os"
+    "time"
 
-	"github.com/gomidi/connect"
-	"github.com/gomidi/mid"
-	driver "github.com/gomidi/rtmididrv"
+    "github.com/gomidi/connect"
+    "github.com/gomidi/mid"
+    driver "github.com/gomidi/rtmididrv"
 )
 
 func must(err error) {
-	if err != nil {
-		panic(err.Error())
-	}
+    if err != nil {
+        panic(err.Error())
+    }
 }
 
 // This example expects the first input and output port to be connected
 // somehow (are either virtual MIDI through ports or physically connected).
 // We write to the out port and listen to the in port.
 func main() {
-	drv, err := driver.New()
-	must(err)
+    drv, err := driver.New()
+    must(err)
 
-	// make sure to close all open ports at the end
-	defer drv.Close()
+    // make sure to close all open ports at the end
+    defer drv.Close()
 
-	ins, err := drv.Ins()
-	must(err)
+    ins, err := drv.Ins()
+    must(err)
 
-	outs, err := drv.Outs()
-	must(err)
+    outs, err := drv.Outs()
+    must(err)
 
-	if len(os.Args) == 2 && os.Args[1] == "list" {
-		printInPorts(ins)
-		printOutPorts(outs)
-		return
-	}
+    if len(os.Args) == 2 && os.Args[1] == "list" {
+        printInPorts(ins)
+        printOutPorts(outs)
+        return
+    }
 
-	in, out := ins[0], outs[0]
+    in, out := ins[0], outs[0]
 
-	must(in.Open())
-	must(out.Open())
+    must(in.Open())
+    must(out.Open())
 
-	wr := mid.WriteTo(out)
+    wr := mid.WriteTo(out)
 
-	// listen for MIDI
-	go mid.NewReader().ReadFrom(in)
+    // listen for MIDI
+    go mid.NewReader().ReadFrom(in)
 
-	{ // write MIDI to out that passes it to in on which we listen.
-		wr.SetChannel(1)
+    { // write MIDI to out that passes it to in on which we listen.
+        wr.SetChannel(1)
 
-		err := wr.NoteOn(60, 100)
-		if err != nil {
-			panic(err)
-		}
+        err := wr.NoteOn(60, 100)
+        if err != nil {
+            panic(err)
+        }
 
-		// Start playing a note
-		note := uint8(60)
-		wr.NoteOn(note, 100)
-		time.Sleep(time.Second * 1)
+        // Start playing a note
+        note := uint8(60)
+        wr.NoteOn(note, 100)
+        time.Sleep(time.Second * 1)
 
-		// Start playing a harmony
-		harmony := note - 2
-		fmt.Println("Reacting to note 60 - playing note 58")
-		wr.NoteOn(harmony, 100)
-		time.Sleep(time.Second * 1)
+        // Start playing a harmony
+        harmony := note - 2
+        fmt.Println("Reacting to note 60 - playing note 58")
+        wr.NoteOn(harmony, 100)
+        time.Sleep(time.Second * 1)
 
-		// Stop note then harmony note
-		wr.NoteOff(note)
-		time.Sleep(time.Nanosecond)
-		wr.NoteOff(harmony)
-		time.Sleep(time.Nanosecond)
+        // Stop note then harmony note
+        wr.NoteOff(note)
+        time.Sleep(time.Nanosecond)
+        wr.NoteOff(harmony)
+        time.Sleep(time.Nanosecond)
 
 
-		// wr.NoteOn(70, 100)
-		// time.Sleep(time.Nanosecond)
-		// wr.NoteOff(70)
-		// time.Sleep(time.Second * 1)
-		// wr.NoteOn(60, 100)
-		// time.Sleep(time.Nanosecond)
-		// wr.NoteOff(60)
-		// time.Sleep(time.Nanosecond)
-	}
+        // wr.NoteOn(70, 100)
+        // time.Sleep(time.Nanosecond)
+        // wr.NoteOff(70)
+        // time.Sleep(time.Second * 1)
+        // wr.NoteOn(60, 100)
+        // time.Sleep(time.Nanosecond)
+        // wr.NoteOff(60)
+        // time.Sleep(time.Nanosecond)
+    }
 }
 
 func printPort(port connect.Port) {
-	fmt.Printf("[%v] %s\n", port.Number(), port.String())
+    fmt.Printf("[%v] %s\n", port.Number(), port.String())
 }
 
 func printInPorts(ports []connect.In) {
-	fmt.Printf("MIDI IN Ports\n")
-	for _, port := range ports {
-		printPort(port)
-	}
-	fmt.Printf("\n\n")
+    fmt.Printf("MIDI IN Ports\n")
+    for _, port := range ports {
+        printPort(port)
+    }
+    fmt.Printf("\n\n")
 }
 
 func printOutPorts(ports []connect.Out) {
-	fmt.Printf("MIDI OUT Ports\n")
-	for _, port := range ports {
-		printPort(port)
-	}
-	fmt.Printf("\n\n")
+    fmt.Printf("MIDI OUT Ports\n")
+    for _, port := range ports {
+        printPort(port)
+    }
+    fmt.Printf("\n\n")
 }
