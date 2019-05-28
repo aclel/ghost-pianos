bf61e707fd357401e9702c2fdf3d5629162521d0
Andrew Cleland <andrew.cleland3@gmail.com>
2019-05-28 20:51:38 +1000

    Read from midi and log messages

--- go.mod
+++ go.mod
@@ -3,5 +3,6 @@ module github.com/aclel/ghost-pianos
 require (
 	github.com/gomidi/connect v0.11.1
 	github.com/gomidi/mid v0.17.0
+	github.com/gomidi/midi v1.6.0
 	github.com/gomidi/rtmididrv v0.0.7
 )
--- main.go
+++ main.go
@@ -1,93 +1,94 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "time"
+	"fmt"
+	"os"
+	"time"
 
-    "github.com/gomidi/connect"
-    "github.com/gomidi/mid"
-    "github.com/aclel/ghost-pianos/bjorklund"
-    driver "github.com/gomidi/rtmididrv"
+	"github.com/aclel/ghost-pianos/bjorklund"
+	"github.com/gomidi/connect"
+	"github.com/gomidi/mid"
+	driver "github.com/gomidi/rtmididrv"
 )
 
 func must(err error) {
-    if err != nil {
-        panic(err.Error())
-    }
+	if err != nil {
+		panic(err.Error())
+	}
 }
 
 // This example expects the first input and output port to be connected
 // somehow (are either virtual MIDI through ports or physically connected).
 // We write to the out port and listen to the in port.
 func main() {
-    drv, err := driver.New()
-    must(err)
+	drv, err := driver.New()
+	must(err)
 
-    // make sure to close all open ports at the end
-    defer drv.Close()
+	// make sure to close all open ports at the end
+	defer drv.Close()
 
-    ins, err := drv.Ins()
-    must(err)
+	ins, err := drv.Ins()
+	must(err)
 
-    outs, err := drv.Outs()
-    must(err)
+	outs, err := drv.Outs()
+	must(err)
 
-    // ./ghost-pianos list
-    if len(os.Args) == 2 && os.Args[1] == "list" {
-        printInPorts(ins)
-        printOutPorts(outs)
-        return
-    }
+	// ./ghost-pianos list
+	if len(os.Args) == 2 && os.Args[1] == "list" {
+		printInPorts(ins)
+		printOutPorts(outs)
+		return
+	}
 
-    in, out := ins[0], outs[0]
+	in, out := ins[0], outs[1]
 
-    must(in.Open())
-    must(out.Open())
+	must(in.Open())
+	must(out.Open())
 
-    wr := mid.WriteTo(out)
+	wr := mid.WriteTo(out)
+	rd := mid.NewReader()
 
-    // listen for MIDI
-    go mid.NewReader().ReadFrom(in)
+	// listen for MIDI
+	go rd.ReadFrom(in)
+	{
+		//write MIDI to out that passes it to in on which we listen.
+		wr.SetChannel(1)
 
-    { // write MIDI to out that passes it to in on which we listen.
-        wr.SetChannel(1)
-
-        rhythm := bjorklund.Bjorklund(13, 5)
-        playRhythm(wr, 70, rhythm)
-    }
+		rhythm := bjorklund.Bjorklund(13, 5)
+		playRhythm(wr, 70, rhythm)
+	}
 }
 
 func playRhythm(wr *mid.Writer, note uint8, rhythm []int) {
-    for i := 0; i < len(rhythm); i++ {
-        pulse := rhythm[i]
+	for i := 0; i < len(rhythm); i++ {
+		pulse := rhythm[i]
 
-        if pulse == 1 {
-            wr.NoteOn(note, 100)
-            time.Sleep(time.Second / 2)
-            wr.NoteOff(note)
-        } else {
-            time.Sleep(time.Second / 2)
-        }
-    }
+		if pulse == 1 {
+			wr.NoteOn(note, 100)
+			time.Sleep(time.Second / 2)
+			wr.NoteOff(note)
+		} else {
+			time.Sleep(time.Second / 2)
+		}
+	}
 }
 
 func printPort(port connect.Port) {
-    fmt.Printf("[%v] %s\n", port.Number(), port.String())
+	fmt.Printf("[%v] %s\n", port.Number(), port.String())
 }
 
 func printInPorts(ports []connect.In) {
-    fmt.Printf("MIDI IN Ports\n")
-    for _, port := range ports {
-        printPort(port)
-    }
-    fmt.Printf("\n\n")
+	fmt.Printf("MIDI IN Ports\n")
+	for _, port := range ports {
+		printPort(port)
+	}
+	fmt.Printf("\n\n")
 }
 
 func printOutPorts(ports []connect.Out) {
-    fmt.Printf("MIDI OUT Ports\n")
-    for _, port := range ports {
-        printPort(port)
-    }
-    fmt.Printf("\n\n")
+	fmt.Printf("MIDI OUT Ports\n")
+	for _, port := range ports {
+		printPort(port)
+	}
+	fmt.Printf("\n\n")
 }
